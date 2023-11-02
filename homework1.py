955f556f193c7b5bd29b8dff38814f7644303af8
CarlosMQV <carlosmarceloqv@gmail.com>
2023-11-01 22:26:54 -0500

    I set up the dataframes of training, validation and testing

--- homework1.py
+++ homework1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #define the location of the csv
-data = 'https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-02-car-price/data.csv'
+data = 'data.csv'
 
 #clean columns data
 df = pd.read_csv(data)
@@ -19,8 +19,39 @@ for col in strings:
 price_logs = np.log1p(df.msrp)
 
 #Plot of the price logarithms
-sns.histplot(price_logs, bins=50)
+#sns.histplot(price_logs, bins=50)
 
 #resume of the null values by columns
-unexisting=df.isnull().sum()
+#unexisting=df.isnull().sum()
+
+n = len(df) #size of the dataset
+n_val = int(n*0.2) #size of the validation dataset (20%)
+n_test = int(n*0.2) #size of the test dataset (20%)
+n_train = n - n_val - n_test #size of the training dataset (~60%)
+
+idx = np.arange(n) #create a 0 to n-1 list of number
+np.random.seed(2)
+np.random.shuffle(idx) #shuffle those numbers
+
+df_train = df.iloc[idx[:n_train]] #dataframe for training
+df_val = df.iloc[idx[n_train:n_train+n_val]] #dataframe for validation
+df_test = df.iloc[idx[n_train+n_val:]] #dataframe for test
+
+#Rest index of dataframes
+df_train = df_train.reset_index(drop=True) 
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+#Set y as array
+y_train = np.log1p(df_train.msrp.values)
+y_val = np.log1p(df_val.msrp.values)
+y_test = np.log1p(df_test.msrp.values)
+
+#Not gonna use them, so we erase for security
+del df_train['msrp']
+del df_val['msrp']
+del df_test['msrp']
+
+
+
 
