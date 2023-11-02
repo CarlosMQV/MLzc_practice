4bc584517728d9ec05731b497a05c856c1a8b0df
CarlosMQV <carlosmarceloqv@gmail.com>
2023-11-01 20:34:51 -0500

    Clean data and first plot

--- homework1.py
+++ homework1.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 data = 'https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-02-car-price/data.csv'
 
 df = pd.read_csv(data)
+df.columns = df.columns.str.lower().str.replace(' ','_')
+strings = list(df.dtypes[df.dtypes == 'object'].index)
 
-df
+for col in strings:
+    df[col] = df[col].str.lower().str.replace(' ','_')
+
+sns.histplot(df.msrp)
 
