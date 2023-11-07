36b75ce1a40508256a703b95ebd93929dcb0dbaa
CarlosMQV <carlosmarceloqv@gmail.com>
2023-11-07 17:12:23 -0500

    Made a feature importance analysis that give us a risk ratio of churning for each categorical value. This is implemented in a for. Also made some annotations to understand and remember better the theory.

--- Classification/Classification-project.py
+++ Classification/Classification-project.py
@@ -41,27 +41,53 @@ del df_train['churn']
 del df_val['churn']
 del df_test['churn']
 
-#Searching for specific values
+#Exploratory data analysis
+
 #df_full_train.isnull().sum() #-> There are no missing values
 #count = df_full_train.churn.value_counts(normalize=True) #-> For counting the number of values of each type
-global_churn_rate = df_full_train.churn.mean()
-
-#Define the numerical variables
-
+global_churn = df_full_train.churn.mean() #Global churn rate
 numerical = ['tenure','monthlycharges','totalcharges']
-
-categorical = ['customerid', 'gender', 'seniorcitizen', 'partner', 'dependents',
+categorical = ['gender', 'seniorcitizen', 'partner', 'dependents',
                'phoneservice', 'multiplelines', 'internetservice','onlinesecurity',
                'onlinebackup', 'deviceprotection', 'techsupport','streamingtv',
                'streamingmovies', 'contract', 'paperlessbilling','paymentmethod']
+unique_values = df_full_train[categorical].nunique() #For unique values into each categorical variable
 
-unique_values = df_full_train[categorical].nunique()
+#Churn rate by gender
+#churn_female = df_full_train[df_full_train.gender == 'female'].churn.mean()
+#churn_male = df_full_train[df_full_train.gender == 'male'].churn.mean()
+#global_churn - churn_female
+#global_churn - churn_male
 
+#Partner count
+#churn_partner = df_full_train[df_full_train.partner == 'yes'].churn.mean()
+#churn_no_partner = df_full_train[df_full_train.partner == 'no'].churn.mean()
+#global_churn - churn_partner
+#global_churn - churn_no_partner
 
+#This analysis for feature importance.
 
+#We can measure it by subtracting the group index minus the global index
+#If the difference is > 0, the group is more likely to churn
+#If the difference is < 0, the group is less likely to churn
+#The greater the difference, the more influential it is
 
+#We can also divide the group index by the global index (risk ratio)
+#If the risk ratio is > 1, the group is more likely to churn
+#If the risk ratio is < 1, the group is less likely to churn
 
+#With a ratio of, for example, 1.01, the churn risk is practically the same as the global one.
+#With a ratio of, for example, 0.75 or less, the risk of churn is low.
+#With a ratio of, for example, 1.22 or more, the risk of churn is high.
+#Each one defines the classification of risks according to what is most convenient.
 
+#Fast way of obtaining the churn substraction
 
-
-
+for c in categorical:
+    print(c)
+    df_group = df_full_train.groupby(c).churn.agg(['mean','count'])
+    df_group['diff'] = df_group['mean'] - global_churn
+    df_group['ratio'] = df_group['mean'] / global_churn
+    print(df_group)
+    print()
+    print()
